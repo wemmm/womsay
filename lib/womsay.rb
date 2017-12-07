de5a829a092397b4e8a3570cc12bc3b23b1df36d
Dom <dom.mcdonnell@gmail.com>
2017-12-07 14:51:46 +0000

    A small contribution

--- lib/womsay.rb
+++ lib/womsay.rb
@@ -6,7 +6,8 @@ module Womsay
     attr_reader :wise_sayings
 
     def initialize
-      @wise_sayings = ["That's a tomorrow problem.", "I'm going into the zone.", "It's Mac o'clock!", "But did you write a test for it?", "Do you have sadness tuna?", "Guess what went wrong with Southeastern Trains today!", "Must be a scope issue.", "I was missing one curlyboi.", "(╯°□°）╯︵ ┻━┻"]
+      @wise_sayings = ["That's a tomorrow problem.", "I'm going into the zone.", "It's Mac o'clock!", "But did you write a test for it?", "Do you have sadness tuna?", "Guess what went wrong with Southeastern Trains today!", "Must be a scope issue.", "I was missing one curlyboi.", "(╯°□°）╯︵ ┻━┻",
+      "If the test isn't there, it can't fail!"]
     end
 
     def say_something
