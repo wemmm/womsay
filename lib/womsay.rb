87e1db7730d616114bdfdb0c485b0f6723795ac3
wemmm <jenny.wem@gmail.com>
2017-12-15 13:32:25 +0000

    w o m s a y s

--- lib/womsay.rb
+++ lib/womsay.rb
@@ -8,7 +8,8 @@ module Womsay
     def initialize
       @wise_sayings = ["That's a tomorrow problem.", "I'm going into the zone.", "It's Mac o'clock!", "But did you write a test for it?", "Do you have sadness tuna?", "Guess what went wrong with Southeastern Trains today!", "Must be a scope issue.", "I was missing one curlyboi.", "(╯°□°）╯︵ ┻━┻",
       "If the test isn't there, it can't fail!", "Could be worse.", "Get those green squares!",
-      "And now my computer is on fire.", "Coffee?", "Documentation is ALWAYS in progress.", "Are these Post-It(tm) brand post-its?"]
+      "And now my computer is on fire.", "Coffee?", "Documentation is ALWAYS in progress.", "Are these Post-It(tm) brand post-its?",
+      "Can you integrate it with Trello?", "If you put a glove in the mud, the mud don't get glovey.", "Experience mediocrity."]
     end
 
     def say_something
